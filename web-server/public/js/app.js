f77b9ac6e53aa5a41c85f355fc8729a653e6c2e9
Ilie Vlad <32937795+vlad434@users.noreply.github.com>
2024-11-24 18:09:44 +0200

    Removed useless code

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -12,14 +12,16 @@ weatherForm.addEventListener("submit", (e) => {
   messageOne.textContent = "Loading...";
   messageTwo.textContent = "";
 
-  fetch(`/weather?address=${location}`).then((response) => {
-    response.json().then((data) => {
-      if (data.error) {
-        messageOne.textContent = data.error;
-      } else {
-        messageOne.textContent = data.location;
-        messageTwo.textContent = data.forecast;
-      }
-    });
-  });
+  fetch(`http://localhost:3000/weather?address=${location}`).then(
+    (response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          messageOne.textContent = data.error;
+        } else {
+          messageOne.textContent = data.location;
+          messageTwo.textContent = data.forecast;
+        }
+      });
+    }
+  );
 });
